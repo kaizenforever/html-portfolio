18b3e8ba8d72052324bd85f1a46a639da94c22b7
Luca <155836913+kaizenforever@users.noreply.github.com>
2024-01-24 01:59:36 +0100

    Delete public/supplements.js

--- public/supplements.js
+++
@@ -1,6 +0,0 @@
-document.write("Questo è un test per provare JavaScript. Io ho un cancro grosso come una pigna nel cervello. GG.");
-
-function retrieveInfo() {
-    var inputText = document.getElementById('myInput').value;
-    alert('The information retrieved is: ' + inputText);
-  }
